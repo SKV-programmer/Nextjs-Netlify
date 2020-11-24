c36e1a79620eee5046abc8ed96be7abda2ac9192
SHIVAM KUMAR VARSHNEY <59953764+SKV-programmer@users.noreply.github.com>
2020-11-25 01:40:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 const { getDisplayName } = require("next/dist/next-server/lib/utils")
 
 function Home() {
-return <div>Welcome, Shivam</div>
+return <div><h1>Welcome, Sakshi</h1></div>
 }
 
 export default Home
